c8ae5ef5399ab1980ff9b0f8e9511594ff80d325
hi-a <hi-a@example.com>
2018-09-25 14:32:26 +0200

    Add support for more of the camera's settings
    - Movie ISO
    - Movie HD remaining_time
    - Exposure compensation
    - Film simulation mode
    - Shutter type
    - Battery level
    - Flash mode
    - Self timer mode
    - Focus mode
    - Recording mode
    - Shooting mode

--- lib/include/settings.hpp
+++ lib/include/settings.hpp
@@ -49,10 +49,26 @@ struct auto_focus_point {
 
 std::string to_string(auto_focus_point const& focus_point);
 
+enum focus_mode {
+  manual_focus = 1,
+  single_autofocus = 32769,
+  continuous_autofocus = 32770
+};
+char const* to_string(focus_mode mode);
+
+enum timer_mode {
+  timer_off = 0,
+  timer_1sec = 1,
+  timer_2sec = 2,
+  timer_5sec = 3,
+  timer_10sec = 4,
+};
+char const* to_string(timer_mode mode);
+
 enum film_simulation_mode {
-  film_simulation_standard = 1,
-  film_simulation_vivid = 2,
-  film_simulation_soft = 3,
+  film_simulation_provia = 1,
+  film_simulation_velvia = 2,
+  film_simulation_astia = 3,
   film_simulation_monochrome = 4,
   film_simulation_sepia = 5,
   film_simulation_pro_neg_hi = 6,
@@ -61,6 +77,12 @@ enum film_simulation_mode {
   film_simulation_monochrome_r_filter = 9,
   film_simulation_monochrome_g_filter = 10,
   film_simulation_classic_chrome = 11,
+  film_simulation_acros = 12,
+  film_simulation_acros_y = 13,
+  film_simulation_acros_r = 14,
+  film_simulation_acros_g = 15,
+  film_simulation_eterna = 16,
+
   film_simulation_count
 };
 
@@ -88,18 +110,70 @@ enum shutter_type {
 };
 char const* to_string(shutter_type shutter);
 
+enum recording_mode {
+  movie_button_unavailable = 0,
+  movie_button_available = 1
+};
+char const* to_string(recording_mode mode);
+
+enum shooting_mode {
+  manual_mode = 1,
+  program_mode = 2,
+  aperture_priority_mode = 3,
+  shutter_priority_mode = 4,
+  auto_mode = 6
+};
+char const* to_string(recording_mode mode);
+
+enum battery_level {
+  critical_battery = 1,
+  one_bar_battery = 2,
+  two_bar_battery = 3,
+  full_battery = 4
+};
+char const* to_string(battery_level level);
+
+enum flash_mode {
+  flash_auto = 1,
+  flash_off = 2,
+  flash_fill_flash = 3,
+  flash_redeye_auto = 4,
+  flash_redeye_fill = 5,
+  flash_external_sync = 6,
+  flash_on = 32769,
+  flash_redeye = 32770,
+  flash_redeye_on = 32771,
+  flash_redeye_sync = 32772,
+  flash_redeye_rear = 32773,
+  flash_slow_sync = 32774,
+  flash_rear_sync = 32775,
+  flash_commander = 32776,
+  flash_disable = 32777,
+  flash_enable = 32778 
+};
+
+char const* to_string(flash_mode flash);
+
 struct camera_settings {
   uint32_t iso;
+  uint32_t movie_iso;
   bool one_div_shutter_speed;
   uint32_t shutter_speed;
+  uint32_t device_error;
+  uint32_t movie_hd_remaining_time;
+  int32_t exposure_compensation;
   white_balance_mode white_balance;
   film_simulation_mode film_simulation;
   auto_focus_point focus_point;
   image_settings image;
   aperture_f_number aperture;
-  int32_t exposure;
   shutter_type shutter;
-  int32_t battery_level;
+  battery_level battery;
+  flash_mode flash;
+  timer_mode self_timer;
+  focus_mode focus;
+  recording_mode recording;
+  shooting_mode shooting;
 };
 
 inline uint64_t shutter_speed_microseconds(camera_settings const& settings) {
--- lib/src/settings.cpp
+++ lib/src/settings.cpp
@@ -7,9 +7,8 @@
 
 namespace fcwt {
 
-#define CASE_RETURN_ENUM_TO_STRING(x) \
-  case x:                             \
-    return #x
+#define CASE_RETURN_ENUM_TO_STRING(x) case x: return #x;
+#define CASE_ASSIGN_AND_BREAK(r, x) case x: r = x; break
 
 char const* to_string(image_format const format) {
   switch (format) {
@@ -47,17 +46,22 @@ char const* to_string(jpeg_aspect const aspect) {
 
 char const* to_string(film_simulation_mode const film_simulation) {
   switch (film_simulation) {
-    CASE_RETURN_ENUM_TO_STRING(film_simulation_standard);
-    CASE_RETURN_ENUM_TO_STRING(film_simulation_vivid);
-    CASE_RETURN_ENUM_TO_STRING(film_simulation_soft);
-    CASE_RETURN_ENUM_TO_STRING(film_simulation_classic_chrome);
+    CASE_RETURN_ENUM_TO_STRING(film_simulation_provia);
+    CASE_RETURN_ENUM_TO_STRING(film_simulation_velvia);
+    CASE_RETURN_ENUM_TO_STRING(film_simulation_astia);
+    CASE_RETURN_ENUM_TO_STRING(film_simulation_monochrome);
+    CASE_RETURN_ENUM_TO_STRING(film_simulation_sepia);
     CASE_RETURN_ENUM_TO_STRING(film_simulation_pro_neg_hi);
     CASE_RETURN_ENUM_TO_STRING(film_simulation_pro_neg_std);
-    CASE_RETURN_ENUM_TO_STRING(film_simulation_monochrome);
     CASE_RETURN_ENUM_TO_STRING(film_simulation_monochrome_y_filter);
     CASE_RETURN_ENUM_TO_STRING(film_simulation_monochrome_r_filter);
     CASE_RETURN_ENUM_TO_STRING(film_simulation_monochrome_g_filter);
-    CASE_RETURN_ENUM_TO_STRING(film_simulation_sepia);
+    CASE_RETURN_ENUM_TO_STRING(film_simulation_classic_chrome);
+    CASE_RETURN_ENUM_TO_STRING(film_simulation_acros);
+    CASE_RETURN_ENUM_TO_STRING(film_simulation_acros_y);
+    CASE_RETURN_ENUM_TO_STRING(film_simulation_acros_r);
+    CASE_RETURN_ENUM_TO_STRING(film_simulation_acros_g);
+    CASE_RETURN_ENUM_TO_STRING(film_simulation_eterna);
   }
   FCWT_UNREACHABLE;
 }
@@ -67,6 +71,60 @@ char const* to_string(shutter_type shutter)
   switch (shutter) {
     CASE_RETURN_ENUM_TO_STRING(mechanical_shutter);
     CASE_RETURN_ENUM_TO_STRING(electronic_shutter);
+  }
+  FCWT_UNREACHABLE; 
+}
+
+char const* to_string(recording_mode mode)
+{
+  switch (mode) {
+    CASE_RETURN_ENUM_TO_STRING(movie_button_unavailable);
+    CASE_RETURN_ENUM_TO_STRING(movie_button_available);
+  }
+  FCWT_UNREACHABLE; 
+}
+
+char const* to_string(shooting_mode mode)
+{
+  switch (mode) {
+    CASE_RETURN_ENUM_TO_STRING(manual_mode);
+    CASE_RETURN_ENUM_TO_STRING(program_mode);
+    CASE_RETURN_ENUM_TO_STRING(aperture_priority_mode);
+    CASE_RETURN_ENUM_TO_STRING(shutter_priority_mode);
+    CASE_RETURN_ENUM_TO_STRING(auto_mode);
+  }
+  FCWT_UNREACHABLE; 
+}
+
+char const* to_string(battery_level level)
+{
+  switch (level) {
+    CASE_RETURN_ENUM_TO_STRING(critical_battery);
+    CASE_RETURN_ENUM_TO_STRING(one_bar_battery);
+    CASE_RETURN_ENUM_TO_STRING(two_bar_battery);
+    CASE_RETURN_ENUM_TO_STRING(full_battery);
+  }
+  FCWT_UNREACHABLE; 
+}
+
+char const* to_string(focus_mode mode)
+{
+  switch (mode) {
+    CASE_RETURN_ENUM_TO_STRING(manual_focus);
+    CASE_RETURN_ENUM_TO_STRING(single_autofocus);
+    CASE_RETURN_ENUM_TO_STRING(continuous_autofocus);
+  }
+  FCWT_UNREACHABLE; 
+}
+
+char const* to_string(timer_mode mode)
+{
+  switch (mode) {
+    CASE_RETURN_ENUM_TO_STRING(timer_off);
+    CASE_RETURN_ENUM_TO_STRING(timer_1sec);
+    CASE_RETURN_ENUM_TO_STRING(timer_2sec);
+    CASE_RETURN_ENUM_TO_STRING(timer_5sec);
+    CASE_RETURN_ENUM_TO_STRING(timer_10sec);
   }
   FCWT_UNREACHABLE; 
 }
@@ -87,7 +145,26 @@ char const* to_string(white_balance_mode const white_balance) {
   FCWT_UNREACHABLE;
 }
 
-#define CASE_ASSIGN_AND_BREAK(r, x) case x: r = x; break
+char const* to_string(flash_mode const flash) {
+  switch (flash) {
+    CASE_RETURN_ENUM_TO_STRING(flash_auto);
+    CASE_RETURN_ENUM_TO_STRING(flash_off);
+    CASE_RETURN_ENUM_TO_STRING(flash_redeye_auto);
+    CASE_RETURN_ENUM_TO_STRING(flash_redeye_fill);
+    CASE_RETURN_ENUM_TO_STRING(flash_external_sync);
+    CASE_RETURN_ENUM_TO_STRING(flash_on);
+    CASE_RETURN_ENUM_TO_STRING(flash_redeye);
+    CASE_RETURN_ENUM_TO_STRING(flash_redeye_on);
+    CASE_RETURN_ENUM_TO_STRING(flash_redeye_sync);
+    CASE_RETURN_ENUM_TO_STRING(flash_redeye_rear);
+    CASE_RETURN_ENUM_TO_STRING(flash_slow_sync);
+    CASE_RETURN_ENUM_TO_STRING(flash_rear_sync);
+    CASE_RETURN_ENUM_TO_STRING(flash_commander);
+    CASE_RETURN_ENUM_TO_STRING(flash_disable);
+    CASE_RETURN_ENUM_TO_STRING(flash_enable);
+  }
+  FCWT_UNREACHABLE;
+}
 
 bool parse_white_balance_mode(uint16_t const value, white_balance_mode& mode) {
   switch (value) {
